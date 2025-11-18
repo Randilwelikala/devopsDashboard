45eb9896b6c5d58af59c0d9b99c5c9decf6b93ca
randilwelikala <randilgimantha646@gmail.com>
2025-11-18 14:25:54 +0530

    delete unwanted files

--- Services/ServerServices.cs
+++ Services/ServerServices.cs
@@ -9,8 +9,14 @@ namespace serverDashboard.Services
 
         public ServerService()
         {
-            string json = File.ReadAllText("Data/serverData.json");
-            _data = JsonSerializer.Deserialize<Root>(json);
+             var path = Path.Combine(AppContext.BaseDirectory, "Data", "serverData.json");
+            Console.WriteLine($"Looking for JSON at: {path}");
+            
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"Could not find the JSON file at {path}");
+
+            var json = File.ReadAllText(path);
+            _data = JsonSerializer.Deserialize<Root>(json) ?? throw new Exception("Failed to deserialize JSON into Root object");
         }
 
         public List<Client> GetClients() => _data.Clients;

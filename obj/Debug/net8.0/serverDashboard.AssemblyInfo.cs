decd28c982fdd7055393510bf61aedfd6187c7e6
randilwelikala <randilgimantha646@gmail.com>
2025-11-20 22:42:58 +0530

    try to change background color

--- obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a7a4d7697c8b18a481fb0ffc7008949a3d24a70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b350de440c19f526a0d990ef100794084a19b33")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
